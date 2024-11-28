824f49b6bbb3cff67376eb1452e983382058ba26
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-11-28 11:13:44 +0530

    example of hoisting

--- 28-11-2024/let_var_const.js
+++ 28-11-2024/let_var_const.js
@@ -1,1 +1,10 @@
-let name = "rakshit"
+// example of hoisting 
+function foo() {
+  console.log(a);  // first it is undefined in execution context 
+}
+var a = 10;
+var a = 15;      // no error as re-declaration is allowed.
+foo();
+
+
+
