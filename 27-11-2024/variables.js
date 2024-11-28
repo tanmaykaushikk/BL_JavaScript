6c4b8107d1f5e476d9287a7d4e7655a87ac1322d
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-11-28 10:56:16 +0530

    first commit

---
+++ 27-11-2024/variables.js
@@ -0,0 +1,31 @@
+/* Topics covered
+        Why and what is JavaScript?
+        why ajax
+        client side / server side
+        execution context
+        variable and datatype
+        let var const
+        es6 features
+        hoisting 
+*/
+
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a(){
+    console.log(10);
+}
+
+function b(){
+    console.log(100);
+}
+
+
+
+
+
+
+
